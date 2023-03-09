6256cee850a695b1588d4310dd7b2cd6ee372545
Mayank Patel <77495279+Mayank-777@users.noreply.github.com>
2023-03-09 12:50:15 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages,setup
+
+
+setup(
+    name='ML_Project',
+    version='0.0.1',
+    author='Mayank',
+    author_email='mayankpatel26022002@gmail.com',
+    packages=find_packages(),
+    install_requires=['pandas','numpy','seaborn']
+)
